7b44d0722d9406f3b05aaf0fb689042d1d03dd4e
David Jensen <tsourdox@icloud.com>
2024-03-13 11:55:18 +0100

    use env in page

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
   return (
     <main className="">
       <h1 className="text-8xl">Site</h1>
-      <p>{new Date().toUTCString()}</p>
+      <p>DATE: {new Date().toUTCString()}</p>
+      <p>NODE: {process.env.NODE_ENV}</p>
+      <p>SECRET: {process.env.SECRET_MESSAGE}</p>
       <Form />
     </main>
   );
